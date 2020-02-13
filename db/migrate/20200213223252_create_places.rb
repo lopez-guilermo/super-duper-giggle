de0b0ba18f8871d469db6524125c8f221d0d3550
guillermo <gl874391@gmail.com>
2020-02-13 22:44:31 +0000

    Initial commit

--- db/migrate/20200213223252_create_places.rb
+++ db/migrate/20200213223252_create_places.rb
@@ -1,7 +1,7 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
-    	t.string :name
+    	t.string :recipe
     	t.text :description
     	t.string :instructions
 
